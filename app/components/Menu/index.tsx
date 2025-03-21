0c203bc421dc0a21a7840d672ccdf12ef1994a3d
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-03-21 02:46:27 -0300

    Estilização do menu

--- app/components/Menu/index.tsx
+++ app/components/Menu/index.tsx
@@ -14,30 +14,21 @@ export function Menu() {
 
     return (
         <header className="menu--container">
-            {isOpen ? (
-                <Image
-                    src={CloseIcon}
-                    alt="ícone de fechar o menu"
-                    onClick={() => setIsOpen(prevState => !prevState)}
-                />
-            ) : (
-                <Image
-                    src={MenuIcon}
-                    alt="ícone de um menu"
-                    onClick={() => setIsOpen(prevState => !prevState)} />
-            )}
+            <Image
+                src={isOpen ? CloseIcon : MenuIcon}
+                alt={isOpen ? 'ícone de fechar o menu' : 'ícone de abrir o menu'}
+                onClick={() => setIsOpen(prevState => !prevState)}
+            />
 
-            {isOpen ? (
-                <nav className={`menu--nav ${isOpen ? 'menu--nav-open' : 'menu--nav-close'}`}>
-                    <ul className="menu--list">
-                        <li className="menu--item">Home</li>
-                        <li className="menu--item">Sobre Nós</li>
-                        <li className="menu--item">Playground</li>
-                        <li className="menu--item">Faça um Orçamento</li>
-                        <li className="menu--item">Contato</li>
-                    </ul>
-                </nav>
-            ) : ''}
+            <nav className={`menu--nav ${isOpen ? 'menu--nav-open' : 'menu--nav-close'}`}>
+                <ul className="menu--list">
+                    <li className="menu--item">Home</li>
+                    <li className="menu--item">Sobre Nós</li>
+                    <li className="menu--item">Playground</li>
+                    <li className="menu--item">Faça um Orçamento</li>
+                    <li className="menu--item">Contato</li>
+                </ul>
+            </nav>
         </header>
     )
 }
