a8d7d2a32234726813121cde70a8bd64acefd87c
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-03-27 22:16:23 -0300

    Criação da seção de Listagem de Produtos

---
+++ app/sections/ProductList/index.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import { Atop } from '@/app/components/AtopFont';
+import './product-list.css';
+import CardImage1 from './img/card-1.jpg';
+import CardImage2 from './img/card-2.jpg';
+import CardImage3 from './img/card-3.jpg';
+
+export function ProductList() {
+  return (
+    <section className="products-list--container">
+      <h1 className={`products--title ${Atop.className}`}>O Playground <span>dos sonhos está aqui!</span></h1>
+
+      <ul className="products-list--grid">
+        <li className="product--item">
+          <div className="product--item--image">
+            <Image src={CardImage1} alt="Imagem do primeiro card" />
+          </div>
+          <div className="product--item--body">
+            <h2>Playground de madeira</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In ipsa non rerum iusto esse perferendis aut velit consequatur laboriosam.</p>
+            <button>Saiba mais</button>
+          </div>
+        </li>
+
+        <li className="product--item">
+          <div className="product--item--image">
+            <Image src={CardImage2} alt="Imagem do primeiro card" />
+          </div>
+          <div className="product--item--body">
+            <h2>Playground de madeira</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In ipsa non rerum iusto esse perferendis aut velit consequatur laboriosam.</p>
+            <button>Saiba mais</button>
+          </div>
+        </li>
+
+        <li className="product--item">
+          <div className="product--item--image">
+            <Image src={CardImage3} alt="Imagem do primeiro card" />
+          </div>
+          <div className="product--item--body">
+            <h2>Playground de madeira</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In ipsa non rerum iusto esse perferendis aut velit consequatur laboriosam.</p>
+            <button>Saiba mais</button>
+          </div>
+        </li>
+      </ul>
+
+    </section>
+  );
+}
