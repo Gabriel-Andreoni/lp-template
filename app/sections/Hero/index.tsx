d15652c67b23f29e43b7584b58245a2b17af59c3
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-04-23 22:49:01 -0300

    Update index.tsx

--- app/sections/Hero/index.tsx
+++ app/sections/Hero/index.tsx
@@ -1,12 +1,10 @@
 import { Atop } from '@/app/components/AtopFont';
 import './hero.css';
-import { Menu } from '@/app/components/Menu';
 
 
 export function Hero() {
     return (
         <section className="hero--container">
-            <Menu />
             <div className="hero--content">
                 <h1 className={`hero--content--title ${Atop.className}`}>Crie <span>Memórias Incríveis</span></h1>
                 <p className="hero--content--text">Transforme o espaço da sua escola, condomínio ou parque em um lugar mágico! Com playgrounds seguros, duráveis e cheios de diversão, oferecemos experiências que encantam crianças e tranquilizam adultos. Conheça nossas soluções e crie memórias que vão durar para sempre.</p>
