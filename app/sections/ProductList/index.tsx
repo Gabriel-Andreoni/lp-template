f99b668ab1e4f0f95305bdb71920ebbd4eb0d0e0
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-05-26 19:11:23 -0300

    Update index.tsx

--- app/sections/ProductList/index.tsx
+++ app/sections/ProductList/index.tsx
@@ -5,6 +5,24 @@ import CardImage1 from './img/card-1.jpg';
 import CardImage2 from './img/card-2.jpg';
 import CardImage3 from './img/card-3.jpg';
 
+const clientImages = [
+  {
+    id: 1,
+    src: CardImage1,
+    alt: 'Imagem do Produto 1'
+  },
+  {
+    id: 2,
+    src: CardImage2,
+    alt: 'Imagem do Produto 2'
+  },
+  {
+    id: 3,
+    src: CardImage3,
+    alt: 'Imagem do Produto 3'
+  }
+]
+
 
 export function ProductList() {
   return (
@@ -12,24 +30,17 @@ export function ProductList() {
       <h1 className={`products--title`}>O Playground <span>dos sonhos está aqui!</span></h1>
 
       <ul className="products-list--grid">
-        <ProductCard
-          image={CardImage1}
-          title="Playground de Madeira"
-          description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          link="/product/1"
-        />
-        <ProductCard
-          image={CardImage2}
-          title="Playground de Madeira"
-          description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          link="/product/2"
-        />
-        <ProductCard
-          image={CardImage3}
-          title="Playground de Madeira"
-          description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          link="/product/3"
-        />
+        {clientImages.map((client: any, index: number) => {
+          return (
+            <ProductCard
+              key={index}
+              image={client.src}
+              title={"Playground de Madeira"}
+              description={`lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`}
+              link={`#`}
+            />
+          )
+        })}
       </ul>
 
     </section>
